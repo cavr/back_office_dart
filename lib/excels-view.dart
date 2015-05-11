c385c025c3406e9d2b83edbf8f5145dea75e1aac
Carlos <carlos.villaroca@yump.com>
2015-05-11 11:08:23 +0200

    Mensaje de error Reporting

--- lib/excels-view.dart
+++ lib/excels-view.dart
@@ -6,19 +6,17 @@ import 'user-session.dart' as globals;
 import 'dart:html';
 
 @CustomTag('excels-view')
-class ExcelsView extends PolymerElement{
-  
+class ExcelsView extends PolymerElement {
   ExcelsView.created() : super.created();
-  
+
   @observable String programId;
-  
+
   void loadDataMining(Event e) {
     var basic = "Basic " + window.sessionStorage["token"];
     Map header = {'Authorization': basic};
     print("Cargando Data Mining");
     HttpRequest
-        .request(globals.dataminingUrl,
-            requestHeaders: header)
+        .request(globals.dataminingUrl, requestHeaders: header)
         .then((HttpRequest req) {
 
       //Map data = JSON.decode(req.response);
@@ -28,29 +26,30 @@ class ExcelsView extends PolymerElement{
     }).catchError((onError) {
       printMessage(onError.target.responseText);
     });
-   
   }
-  
+
   void loadReporting(Event e) {
-      var basic = "Basic " + window.sessionStorage["token"];
-      Map header = {'Authorization': basic};
-      print("Cargando Reporting");
-      HttpRequest
-          .request(globals.reportingUrl + programId,
-              requestHeaders: header)
-          .then((HttpRequest req) {
+    if (programId == null) {
+      printMessage("PROGRAM ID BLANK");
+      return;
+    }
+    var basic = "Basic " + window.sessionStorage["token"];
+    Map header = {'Authorization': basic};
+    print("Cargando Reporting");
+    HttpRequest
+        .request(globals.reportingUrl + programId, requestHeaders: header)
+        .then((HttpRequest req) {
 
-        //Map data = JSON.decode(req.response);
+      //Map data = JSON.decode(req.response);
 
-        print(req.response);
-        printMessage(req.response);
-      }).catchError((onError) {
-        printMessage(onError.target.responseText);
-      });
+      print(req.response);
+      printMessage(req.response);
+    }).catchError((onError) {
+      printMessage(onError.target.responseText);
+    });
   }
   void printMessage(String response) {
-      shadowRoot
-           .querySelector('#response')
-           .innerHtml = ("<br><br><br>><pre>" + response + "</pre>");
-     }
+    shadowRoot.querySelector('#response').innerHtml =
+        ("<br><br><br>><pre>" + response + "</pre>");
+  }
 }
